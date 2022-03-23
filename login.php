133c6f97db66c4c48ffb552235131d704f5db0fa
elzig <72elzig@gmail.com>
2022-03-23 19:59:50 +0100

    Page de login fini

--- login.php
+++ login.php
@@ -1,69 +1,35 @@
-<html>
-    <head>
-       <meta charset="utf-8">
-        <!-- importer le fichier de style -->
-        <link rel="stylesheet" href="style/login_test.css" media="screen" type="text/css" />
-        <link rel="icon" href="img/favicon.ico" />
-    </head>
+<?php include("includes/head.html") ?>
 
-    <body>
-        <div id="container-connection">
-            <!-- zone de connexion -->
-            
-            <form action="verification.php" method="POST">
-                <h1>Connexion</h1>
+<head>
+    <link rel="stylesheet" href="style/login.css">
+</head>
 
-                
+<body class="text-center">
+    <div class="container">
+        <h2 class="mb-3 fw-normal">Connexion</h2>
 
-                <label><b>Email</label>
-                <input type="email" placeholder="Entrez votre email" name="email" required>
-
-                <label><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrez votre mot de passe" name="password" required>
-
-                <button type="submit" id='submit' value='LOGIN' >Se connecter</button>
-                <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+        <?php
+            if(isset($_GET['erreur'])){
+                $err = $_GET['erreur'];
+                if($err==1 || $err==2){
+                    echo '<div class="alert alert-danger" role="alert">Utilisateur ou mot de passe incorrect</div>';
                 }
-                if(isset($_GET['inscri'])){
-                    $ins = $_GET['inscri'];
-                    if($ins==1 )
-                       echo "<script> alert(\"Vous êtes bien inscrit, vous pouvez désormais vous connecter\")</script>";
-                }
-                ?>
-            </form>
-        </div>
+            }
+        ?>
 
-        <div id="container-inscription">
-            <!-- zone d'inscription -->
-            
-            <form action="inscription.php" method="GET">
-                <h1>Inscription</h1>
+        <form action="verification.php" method="POST">
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="Entrez votre email" name="email" required>
+                <label for="floatingInput">Email</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingInput" placeholder="Entrez votre mot de passe" name="password" required>
+                <label for="floatingInput">Mot de passe</label>
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">Se connecter</button>
+        </form>
+        <a class="btn btn-warning mb-2 mt-2" href="register.php">S'enregistrer</a>
+    </div>
+</body>
 
-                <label><b>Nom</b></label>
-                <input type="text" placeholder="Entrez votre nom" name="nom" required>
-
-                <label><b>Prénom</b></label>
-                <input type="text" placeholder="Entrez votre prenom" name="prenom" required>
-
-                <label><b>Email</b></label>
-                <input type="email" placeholder="Entrez votre email" name="emaili" required>
-
-                <label><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrez votre mot de passe" name="passwordi" required>
-
-                <button type="submit" id='submit' value='LOGIN' >S'inscrire</button>
-                <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-                }
-                ?>
-            </form>
-        </div>
-    </body>
-</html>
+<?php include("includes/foot.html"); ?>
